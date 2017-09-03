a65a1197792e0543a9a37fc44ff99f64ce7e811f
Zoran Antolovic <zoran.antolovic.os@gmail.com>
2017-09-03 20:33:07 +0200

    Add redis cache

--- implementation/symfony/src/AppBundle/Controller/NewsPortalController.php
+++ implementation/symfony/src/AppBundle/Controller/NewsPortalController.php
@@ -2,34 +2,39 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Category;
 use AppBundle\Entity\Post;
+use Doctrine\ORM\EntityRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class NewsPortalController extends Controller
 {
+    private function saveToCache($key, $value)
+    {
+        $data = $this->get('cache.app')->getItem($key);
+        $data->set($value);
+        $this->get('cache.app')->save($data);
+    }
+
+    private function loadFromCache($key)
+    {
+        $cacheEngine = $this->get('cache.app');
+
+        $cacheData = $cacheEngine->getItem($key);
+        if (!$cacheData->isHit()) {
+            return false;
+        }
+
+        return $cacheData->get();
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $category1 = $this->getDoctrine()
-            ->getRepository(Category::class)
-            ->findOneBy(['title' => 'category1']);
-
-        $category2 = $this->getDoctrine()
-            ->getRepository(Category::class)
-            ->findOneBy(['title' => 'category2']);
-
-        $category1Posts = $this->getDoctrine()
-            ->getRepository(Post::class)
-            ->findBy(['category' => $category1], ['createdAt' => 'DESC'], 15);
-
-        $category2Posts = $this->getDoctrine()
-            ->getRepository(Post::class)
-            ->findBy(['category' => $category2], ['createdAt' => 'DESC'], 15);
+        $category1Posts = $this->loadPosts('category1');
+        $category2Posts = $this->loadPosts('category2');
 
         return $this->render('@App/homepage.html.twig', [
             'category1Posts' => $category1Posts,
@@ -40,8 +45,36 @@ class NewsPortalController extends Controller
     /**
      * @Route("/test", name="test")
      */
-    public function testAction(Request $request)
+    public function testAction()
     {
         phpinfo();
+    }
+
+    private function loadPostsFromDb($categoryName, $maxResults = 15)
+    {
+        /** @var EntityRepository $repo */
+        $repo = $this->getDoctrine()->getRepository(Post::class);
+
+        $qb = $repo
+            ->createQueryBuilder('p')
+            ->innerJoin('p.category', 'c')
+            ->where('c.title = :categoryName')
+            ->setParameter('categoryName', $categoryName)
+            ->setMaxResults($maxResults)
+            ->orderBy('p.createdAt', 'DESC');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    private function loadPosts($categoryName, $maxResults = 15)
+    {
+        $cachedData = $this->loadFromCache($categoryName);
+        if (false === $cachedData) {
+            $cachedData = $this->loadPostsFromDb($categoryName, $maxResults);
+            $this->saveToCache($categoryName, $cachedData);
+        }
+
+        return $cachedData;
+
     }
 }
